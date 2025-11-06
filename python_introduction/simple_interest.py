5f4b03e37127eb1fd6f8715fa932b57e597c752e
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-06 13:47:18 +0100

    fix simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,6 @@ principal =int(1000)
 rate = float(0.05)
 time = int(3)
 #Calculate the simple interest(I = P * R * T)
-simple_interest = principal * rate * time
+interest = principal * rate * time
 #Print the result
-print("The simple interest is : ", simple_interest)
+print("The simple interest is : ", interest)
