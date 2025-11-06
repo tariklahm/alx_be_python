fd3fbea79e7e3ef105cbfe465afdefff7e6718df
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-06 12:40:51 +0100

    Add basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# Assignements
+number1 = 10    
+number2 = 5
+# Results calculations
+addition_result = number1 + number2
+subtruction_result = number1 - number2
+multiplication_result = number1 * number2
+# Print the results of each operation
+print("Addition of ",number1, "and", number2 ,"is", addition_result)
+print("Subtraction of ",number1, "and", number2 ,"is", subtruction_result)
+print("Multiplication of ",number1, "and", number2 ,"is", multiplication_result)
