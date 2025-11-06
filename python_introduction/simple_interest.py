6ce52551c4d7f3361a4aa3ac47f8cc1faabd3296
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-06 13:20:01 +0100

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#variable assignement(principle in $, rate %, time in years)
+principle =int(1000)
+rate = float(0.05)
+time = int(3)
+#Calculate the simple interest(I = P * R * T)
+simple_interest = principle * rate * time
+#Print the result
+print("The simple interest is : ", simple_interest)
