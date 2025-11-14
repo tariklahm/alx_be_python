ac3e1880bc58e185dd42b2265a24391222457825
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-14 15:18:25 +0100

    Adjust the print()

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,6 +3,6 @@ rows =int(input("Enter the size of the pattern: "))
 i=0
 while i < rows :
     for j in range(1,rows+1):
-        print(f"*",end="")
+        print("*",end="")
     print()
     i+=1
