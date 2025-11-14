74fe54ac9a9f675d5f9f3404a2fdc316ed7a5488
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-14 11:00:28 +0100

    Adjust the error for another check

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,10 @@
-weather = input("What's the weather like today? (sunny/rainy/cold):")
-if weather.lower() == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = weather.lower()
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather.lower() == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather.lower() == "cold":
-    print("ake sure to wear a warm coat and a scarf.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else :
     print("Sorry, I don't have recommendations for this weather.")
