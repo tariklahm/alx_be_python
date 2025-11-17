82a8b5f4805cb400b705eefd72a30fc4f3296803
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-17 01:17:49 +0100

    Change the variables in the calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,20 +1,20 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case '+' :
-        result = first_number + second_number
+        result = num1 + num2
         print(f"The result is {result}")
     case '-' :
-        result = first_number - second_number
+        result = num1 - num2
         print(f"The result is {result}")
     case '*' :
-        result = first_number * second_number
+        result = num1 * num2
         print(f"The result is {result}")
     case '/' :
-        if second_number == 0 :
+        if num2 == 0 :
             print("Cannot divide by zero.")
         else:
-             result = first_number / second_number
+             result = num1 / num2
              print(f"The result is {result}")
 
