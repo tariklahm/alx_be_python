17d86ee47fa9aca511808a07e8fc0e55a929069a
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-20 15:34:16 +0100

    Change the mach-case with if-elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,17 @@
-def perform_operation(num1,num2,operation):
-    match operation: 
-        case 'add': return num1 + num2
-        case 'subtract': return num1 - num2
-        case 'multiply': return num1 * num2
-        case 'divide': 
-            if num2 == 0:
-                print("ERROR !! we can't divide i number by 0")
-                return None
-            return num1 / num2
-        
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
 
+    elif operation == 'subtract':
+        return num1 - num2
 
+    elif operation == 'multiply':
+        return num1 * num2
 
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: we can't make division by zero"
+        return num1 / num2
 
-
-
-
+    else:
+        return "Invalid operation"
