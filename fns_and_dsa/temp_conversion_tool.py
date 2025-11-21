6084c5592f5c12c4e917bcc326a29e5358e0e6d9
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-21 15:37:45 +0100

    Add the  invalid temp statement

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,17 +2,19 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    
+
     return (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
     
-def convert_to_fahrenheit(celsius):
-    
-    print(f"")
+def convert_to_fahrenheit(celsius):    
+
     return (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 
-
-temp_to_convert = float(input("Enter the temperature to convert: "))
+try:
+    temp_to_convert = float(input("Enter the temperature to convert: "))
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
+    exit()
 celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 match celsius_or_fahrenheit:
     case "F":
