75f7cf5d67c8cd0b4053d5c2d64a59b3b34dd759
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-11-30 10:31:49 +0100

    Adjust the result form

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,5 +10,5 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return ("Error: Cannot divide by zero.")
 
-    return f"The result of the division is {result:.2f}"
+    return f"The result of the division is {result:.1f}"
 
