5ea82ab661776bae98b06e03a5b51f51fe6fe8ae
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-12-07 23:36:26 +0100

    Add book_class

---
+++ oop/book_class.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title:str, author:str, year:int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"    
+    
+    def __del__(self):
+        return print(f"Deleting {self.title}")
