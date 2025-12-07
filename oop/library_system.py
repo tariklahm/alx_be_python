b0578296a6b95a4ff8f4cfd0d9720e733a7f6d6f
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-12-07 23:47:50 +0100

    Add liberary system

---
+++ oop/library_system.py
@@ -0,0 +1,28 @@
+# Base class
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+# Derived class: EBook
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+# Derived class: PrintBook
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
